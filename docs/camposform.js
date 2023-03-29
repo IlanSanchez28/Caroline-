9eaa08cb284013b79b03d5b8d3eb9a2437297821
Ilán Sánchez <ilancsanchezor@gmail.com>
2023-03-29 02:10:32 -0600

    modal en formulario

--- docs/camposform.js
+++ docs/camposform.js
@@ -1,49 +1,40 @@
-let nombre = document.getElementById("nombre");
-let email = document.getElementById("email");
-let pass = document.getElementById("pass");
-let form = document.getElementById("form");
-let parrafo = document.getElementById("alertaRegistro");
+const nombre = document.getElementById("nombre")
+const email = document.getElementById("email")
+const pass = document.getElementById("pass")
+const form = document.getElementById("formul")
+const parrafo = document.getElementById("warnings")
 
-form.addEventListener('submit', e=>{
+form.addEventListener("submit", e=>{
     e.preventDefault()
-    let alerta = "";
-    let valido = false;
-    parrafo.innerHTML = "";
-    
+    let warnings = ""
+    let entrar = false
     let regexNombre = /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/g;
-    let regexpEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
     let regexPass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*-_])[A-Za-z\d@$!%*?&-_]{8,}$/;
-
+    parrafo.innerHTML = ""
     if(!regexNombre.test(nombre.value) || nombre.value.length <= 2 || nombre.value.length == 0 || nombre.value.length > 20){
-        alerta += `<h3>¡Nombre no valido!</h3> <br>`;
-        valido = true;
+        warnings += `¡Nombre no valido!<br>`;
+        entrar = true;
     }//validaciónNombre
-
-    if(!regexpEmail.test(email.value) || email.value.length == 0 || email.value.length >= 200){
-        alerta += `<h3>¡Correo electrónico no valido!</h3> <br>`;
-        valido = true;
+    if(!regexEmail.test(email.value) || email.value.length == 0 || email.value.length >= 200){
+        warnings += `¡Correo electrónico no valido!<br>`;
+        entrar = true;
     }//validaciónCorreoElectrónico
-
     if(!regexPass.test(pass.value) || pass.value.length == 0 || pass.value.length > 20){
-        alerta += `<h3>¡Contraseña no valida! </h3><br>`;
-        valido = true;
-    } 
+        warnings += `¡Contraseña no valida!<br>`;
+        entrar = true;
+    } //validaciónContraseña
 
-    if(valido){
-        parrafo.innerHTML += `
-        <div class="alert alert-danger" role="alert">
-            ${alerta}
-        </div>`; 
+    if(entrar){
+        parrafo.innerHTML += `<div class="alert alert-danger" role="alert">
+        ${warnings}
+      </div>`;
+    }else{
+        parrafo.innerHTML += `<div class="alert alert-success" role="alert">
+         <h3>Formulario correcto <br> ¡Enviado! </h3>
+      </div>`
+      form.reset();
     }
-
-    if(!valido){
-        parrafo.innerHTML += `
-        <div class="alert alert-success" role="alert">
-            <h3> ¡Tu registro ha sido exitoso! </h3>
-        </div>`;
-    }
-
-
-
-      
 })
+
+
